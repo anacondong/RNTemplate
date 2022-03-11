efdb124cd1d2869d7e48996fa1480392656c1727
anacondong <westkitseo@gmail.com>
2022-03-12 00:11:39 +0700

    create api

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({ message: 'anacondong!' })
+  })
+
+app.get('/login', (req, res) => {
+  if(req.query.username == "test" && req.query.password == "test"){
+    res.json({ user: {name: "test", password:"test", isAuth: true} })
+  } else {
+    res.json({ error: {msg: req.query.username+" login failed"} })
+  }
+})
+
+app.listen(9000, () => {
+  console.log('Application is running on port 9000')
+})
